ec18386a46b177b5549e2c904ff977082985bb10
ketan <ketanpardhi04@gmail.com>
2024-05-09 20:46:35 +0530

    basic frontend template

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import { Routes, Route} from "react-router-dom";
+import SignUp from './Components/Login_SignUp/SignUp';
+import Homepage from './Components/Home/Homepage';
+import Login from './Components/Login_SignUp/Login';
+import NavBar from './Components/Navbar/Navbar';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      <NavBar/>
+      <Routes>
+        <Route path='/' element={<Homepage/>}></Route>
+        <Route path='/signup' element={<SignUp/>}></Route>
+        <Route path='/login' element={<Login/>}></Route>
+      </Routes>
+
+    </div>
+  );
 }
 
-export default App
+export default App;
